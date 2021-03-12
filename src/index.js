b07b5959203b6901f19541c091241390cee52210
Nastia <70321720+veroivska@users.noreply.github.com>
2021-03-12 20:10:30 +0200

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        var i;
+        var min = array[0];
+        for (i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        var i;
+        var max = array[0];
+        for (i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        var avg;
+        var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        avg = sum / array.length;
+        return avg;
+    }
+};
